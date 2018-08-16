28bc8314d659b3bfb0b08792ea614e81090dd1c1
Amit Kumar <amit1509kumar@gmail.com\>
2018-08-16 20:18:52 +0530

    Problem 151 Reverse words in string

---
+++ LeetCodeSolutions/LeetCodeSolutions/151. Reverse Words in a String/ReverseWordsInString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions._151._Reverse_Words_in_a_String
+{
+    public class ReverseWordsInString
+    {
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return string.Empty;
+
+            string[] st = s.Trim().Split(' ').Select(a => a.Trim()).ToArray();
+
+            var result = string.Empty;
+
+            //reverse words in array
+            for (int i = 0, j = st.Length - 1; i < j; i++, j--)
+            {
+                var temp = st[i];
+                st[i] = st[j];
+                st[j] = temp;
+            }
+
+            //format string
+            for (int k = 0; k < st.Length; k++)
+            {
+                if (string.IsNullOrEmpty(st[k])) continue;
+                if (k < st.Length - 1)
+                    result += st[k] + " ";
+                else
+                    result += st[k];
+            }
+
+
+            return result;
+
+        }
+    }
+}
