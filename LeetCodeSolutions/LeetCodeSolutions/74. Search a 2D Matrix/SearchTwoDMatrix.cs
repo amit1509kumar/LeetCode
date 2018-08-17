c5df542dd5ae9119cb5b70b498a4fec18c7ded3f
Amit Kumar <amit1509kumar@gmail.com\>
2018-08-18 02:45:07 +0530

    74. Search a 2D Matrix

---
+++ LeetCodeSolutions/LeetCodeSolutions/74. Search a 2D Matrix/SearchTwoDMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class SearchTwoDMatrix
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            int len = matrix.Length;
+
+            if (len == 0) return false;
+
+            int low = 0;
+            int high = len - 1;
+            int mid = 0;
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            while(low <= high)
+            {
+                mid = (high + low) / 2;
+
+                int rIndex = mid / cols;
+
+                int cIndex = mid % cols;
+
+                if (matrix[rIndex, cIndex] == target)
+                    return true;
+
+                if (matrix[rIndex, cIndex] > target)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            return false;
+
+        }
+    }
+}
--- LeetCodeSolutions/LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/LeetCodeSolutions/Program.cs
@@ -9,7 +9,15 @@ namespace LeetCodeSolutions
     class Program
     {
         static void Main(string[] args)
-        {         
+        {
+
+            var obj = new SearchTwoDMatrix();
+            int[,] matrix = new int[,] { { 1, 3, 5, 7 }, 
+                                        { 10, 11, 16, 20},
+                                        { 23, 30, 34, 50} };
+
+           var val = obj.SearchMatrix(matrix, 51);
+
 
 
         }
