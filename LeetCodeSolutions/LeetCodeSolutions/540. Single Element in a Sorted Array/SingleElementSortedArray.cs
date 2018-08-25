321e3efcb4c8180c8685086cef222cd8f41a8a82
Amit Kumar <amit1509kumar@gmail.com\>
2018-08-26 01:51:43 +0530

    Single element in sorted array

---
+++ LeetCodeSolutions/LeetCodeSolutions/540. Single Element in a Sorted Array/SingleElementSortedArray.cs
@@ -0,0 +1,49 @@
+﻿/*
+ *Single Element in a Sorted Array O(logn) solution 
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class SingleElementSortedArray
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            int len = nums.Length;
+
+            if (nums.Length < 2) return -1;
+
+            if (nums[0] != nums[1]) return nums[0];
+
+            if (nums[len - 1] != nums[len - 2]) return nums[len - 1];
+
+            int left = 0;
+            int right = nums.Length;
+            int mid = 0;
+            while(left < right)
+            {
+                mid = left + (right - left) / 2;
+
+                if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+                    return nums[mid];
+
+                if ((nums[mid] == nums[mid + 1] && mid % 2 == 0 )
+                    || (nums[mid] == nums[mid - 1] && mid % 2 != 0))
+                    left = mid;
+                else
+                    right = mid;
+            }
+
+
+            return -1;
+
+
+
+        }
+    }
+}
--- LeetCodeSolutions/LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/LeetCodeSolutions/Program.cs
@@ -11,13 +11,7 @@ namespace LeetCodeSolutions
         static void Main(string[] args)
         {
 
-            var obj = new SearchTwoDMatrix();
-            int[,] matrix = new int[,] { { 1, 3, 5, 7 }, 
-                                        { 10, 11, 16, 20},
-                                        { 23, 30, 34, 50} };
-
-           var val = obj.SearchMatrix(matrix, 51);
-
+           
 
 
         }
