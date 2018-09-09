5d309d822bf5ce277aabdc193ba0daca9948f9df
Amit Kumar <amit1509kumar@gmail.com\>
2018-09-09 17:08:25 +0530

    129 Sum root to leaf numbers and 713 Subarray product less than k

---
+++ LeetCodeSolutions/LeetCodeSolutions/129. Sum Root to Leaf Numbers/SumRoottoLeafNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class SumRoottoLeafNumbers
+    {
+        private int result = 0;
+        public int SumNumbers(TreeNode root, int sum = 0)
+        {
+
+            if (root == null) return result;
+
+            if (root.left == null && root.right == null)
+                result = result + sum * 10 + root.val;
+
+            SumNumbers(root.left, sum * 10 + root.val);
+
+            SumNumbers(root.right, sum * 10 + root.val);
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutions/LeetCodeSolutions/713. Subarray Product Less Than K/SubarrayProduct.cs
@@ -0,0 +1,31 @@
+﻿/* the idea is to use sliding window technique to
+ * keep multiplication window value smaller than k  */
+namespace LeetCodeSolutions
+{
+   public class SubarrayProduct
+    {
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+
+            int res = 0;
+            int mul = 1;
+
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                mul *= nums[i];
+
+                while (j <= i && mul >= k)
+                {
+                    mul /= nums[j];
+
+                    j++;
+                }
+
+                res += i - j + 1;
+            }
+
+            return res;
+
+        }
+    }
+}
