98befd18c7981ddf301acc8e12f3004751ba1ce5
Amit Kumar <amit1509kumar@gmail.com\>
2018-09-23 16:34:24 +0530

    Solution of problem Kth Largest Element in an Array

---
+++ LeetCodeSolutions/LeetCodeSolutions/Kth Largest Element in an Array/KthLargestElementInAnArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class KthLargestElementInAnArray
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            var arr = new int[100000];
+            var freq = new int[100000];
+
+            int min = nums.Min();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[nums[i] - min] = nums[i];
+                freq[nums[i] - min]++;
+            }
+
+
+            for (int i = freq.Length - 1; i >= 0; i--)
+            {
+                if (freq[i] == 0) continue;
+
+                k -= freq[i];
+
+                if (k <= 0)
+                    return arr[i];
+            }
+
+            return 0;
+        }
+    }
+}
--- LeetCodeSolutions/LeetCodeSolutions/Student Attendance Record I/StudentAttendanceRecordI.cs
+++ LeetCodeSolutions/LeetCodeSolutions/Student Attendance Record I/StudentAttendanceRecordI.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCodeSolutions.Student_Attendance_Record_I
+namespace LeetCodeSolutions
 {
     public class StudentAttendanceRecordI
     {
