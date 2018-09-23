5c36520c8521498a7ac66f911552c6eb9734c634
Amit Kumar <amit1509kumar@gmail.com\>
2018-09-23 16:25:35 +0530

    551. Student Attendance Record I

---
+++ LeetCodeSolutions/LeetCodeSolutions/Student Attendance Record I/StudentAttendanceRecordI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Student_Attendance_Record_I
+{
+    public class StudentAttendanceRecordI
+    {
+        public bool CheckRecord(string s)
+        {
+
+            int countA = 0;
+            int countL = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                //count absents 'A'
+                if (s[i] == 'A')
+                    countA++;
+
+                //count continuous late 'L' 
+                if (s[i] == 'L')
+                    countL++;
+                else
+                    countL = 0;
+
+                //if student late more than 2 times return false
+                if (countL > 2)
+                    return false;
+            }
+
+            //Last condition to check if absent count exceed more than 1. 
+            return countA < 2;
+
+        }
+    }
+}
