cc408055072bf7e158c009c383aa65ae55822a8b
Amit Kumar <amit1509kumar@gmail.com\>
2020-04-02 15:22:23 +0530

    PriorityQueue data strcture in C#

---
+++ LeetCodeSolutions/LeetCodeSolutions/Data Structure/PriorityQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    class PriorityQueue<T>
+    {
+        private readonly List<T> heap;
+        private readonly Comparison<T> compare;
+        private int size;
+        public PriorityQueue() : this(Comparer<T>.Default) { }
+        public PriorityQueue(IComparer<T> comparer) : this(16, comparer.Compare) { }
+        public PriorityQueue(Comparison<T> comparison) : this(16, comparison) { }
+        public PriorityQueue(int capacity, Comparison<T> comparison)
+        {
+            this.heap = new List<T>(capacity);
+            this.compare = comparison;
+        }
+        public void Enqueue(T item)
+        {
+            this.heap.Add(item);
+            var i = size++;
+            while (i > 0)
+            {
+                var p = (i - 1) >> 1;
+                if (compare(this.heap[p], item) <= 0)
+                    break;
+                this.heap[i] = heap[p];
+                i = p;
+            }
+            this.heap[i] = item;
+
+        }
+        public T Dequeue()
+        {
+            var ret = this.heap[0];
+            var x = this.heap[--size];
+            var i = 0;
+            while ((i << 1) + 1 < size)
+            {
+                var a = (i << 1) + 1;
+                var b = (i << 1) + 2;
+                if (b < size && compare(heap[b], heap[a]) < 0) a = b;
+                if (compare(heap[a], x) >= 0)
+                    break;
+                heap[i] = heap[a];
+                i = a;
+            }
+            heap[i] = x;
+            heap.RemoveAt(size);
+            return ret;
+        }
+        public T Peek() { return heap[0]; }
+        public int Count { get { return size; } }
+        public bool Any() { return size > 0; }
+    }
+}
