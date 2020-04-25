bf1716945b887c517099e117549fbe1d5e1567ad
Amit Kumar <amit1509kumar@gmail.com\>
2020-04-25 18:13:05 +0530

    376. Wiggle Subsequence 1372. Longest ZigZag Path in a Binary Tree

---
+++ LeetCodeSolutions/LeetCodeSolutions/1130 Minimum Cost Tree From Leaf Values/MinimumCostTreeFromLeafValues.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions._1130_Minimum_Cost_Tree_From_Leaf_Values
+{
+   public class MinimumCostTreeFromLeafValues
+    {
+        public int LongestZigZag(TreeNode root)
+        {
+            var q = new Queue<object[]>();
+            q.Enqueue(new object[] { root, 0, "root" });
+
+            int ans = 0;
+
+            while (q.Count > 0)
+            {
+                var c = q.Count;
+
+                for (int i = 0; i < c; i++)
+                {
+                    var node = q.Dequeue();
+
+                    ans = Math.Max(ans, Convert.ToInt32(node[1]));
+
+                    var treeNode = node[0] as TreeNode;
+                    if (treeNode.left != null)
+                    {
+                        if (node[2] == "right" || node[2] == "root")
+                            q.Enqueue(new object[] { treeNode.left, (int)node[1] + 1, "left" });
+                        else
+                            q.Enqueue(new object[] { treeNode.left, 1, "left" });
+
+                    }
+
+                    if (treeNode.right != null)
+                    {
+                        if (node[2] == "left" || node[2] == "root")
+                            q.Enqueue(new object[] { treeNode.right, (int)node[1] + 1, "right" });
+                        else
+                            q.Enqueue(new object[] { treeNode.right, 1, "right" });
+
+                    }
+
+                }
+
+
+
+            }
+
+            return ans;
+
+        }
+
+
+    }
+}
---
+++ LeetCodeSolutions/LeetCodeSolutions/376. Wiggle Subsequence/WiggleSubsequence.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions._376._Wiggle_Subsequence
+{
+    public class WiggleSubsequence
+    {
+              
+        public int WiggleMaxLength(int[] nums)
+        {
+
+            if (nums == null || nums.Length < 1) return 0;
+
+            return Math.Max(Check(nums, 0), Check(nums, 1)) + 1;
+        }
+
+
+        public int Check(int[] nums, int last)
+        {
+            int ans = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1]) continue;
+
+                int currBit = nums[i] - nums[i - 1] < 0 ? 0 : 1;
+
+                if (currBit == (last ^ 1)) ans++;
+
+                last = currBit;
+
+            }
+
+            return ans;
+
+        }
+    }
+}
+
